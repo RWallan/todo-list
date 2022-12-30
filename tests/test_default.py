3441d10d7b30a6d413c977551195b690ef03892d
RWallan <3am.richardwallan@gmail.com>
2022-12-30 11:36:58 -0300

    [TESTS] Tests to new features

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,26 +1,27 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from task_manager.manager import TASKS, app
+from task_manager.app.models.tasks import TASKS
+from task_manager.main import app
 
 
 def test_list_task_must_return_200_status_code():
     client = TestClient(app)
-    response = client.get("/tasks")
+    response = client.get("/api/v1/tasks")
 
     assert response.status_code == status.HTTP_200_OK
 
 
 def test_list_task_format_must_return_a_json():
     client = TestClient(app)
-    response = client.get("/tasks")
+    response = client.get("/api/v1/tasks")
 
     assert response.headers["Content-type"] == "application/json"
 
 
 def test_list_task_must_return_a_list():
     client = TestClient(app)
-    response = client.get("/tasks")
+    response = client.get("/api/v1/tasks")
 
     assert isinstance(response.json(), list)
 
@@ -36,7 +37,7 @@ def test_list_task_return_must_has_id():
     )
 
     client = TestClient(app)
-    response = client.get("/tasks")
+    response = client.get("/api/v1/tasks")
 
     assert "id" in response.json().pop()
 
@@ -54,7 +55,7 @@ def test_list_task_return_must_has_title():
     )
 
     client = TestClient(app)
-    response = client.get("/tasks")
+    response = client.get("/api/v1/tasks")
 
     assert "title" in response.json().pop()
 
@@ -72,7 +73,7 @@ def test_list_task_return_must_has_description():
     )
 
     client = TestClient(app)
-    response = client.get("/tasks")
+    response = client.get("/api/v1/tasks")
 
     assert "description" in response.json().pop()
 
@@ -90,8 +91,109 @@ def test_list_task_return_must_has_status():
     )
 
     client = TestClient(app)
-    response = client.get("/tasks")
+    response = client.get("/api/v1/tasks")
 
     assert "status" in response.json().pop()
+
+    TASKS.clear()
+
+
+def test_tasks_must_accept_post():
+    client = TestClient(app)
+    response = client.post("/api/v1/tasks")
+
+    assert response.status_code != status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+def test_post_task_must_have_a_title():
+    client = TestClient(app)
+    response = client.post("/api/v1/tasks")
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_title_must_contain_between_3_50_characters():
+    client = TestClient(app)
+    response = client.post("/api/v1/tasks", json={"title": 2 * "*"})
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    response = client.post("/api/v1/tasks", json={"title": 51 * "*"})
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_post_task_must_have_a_description():
+    client = TestClient(app)
+    response = client.post("/api/v1/tasks", json={"title": "A title"})
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_description_must_max_length_of_140_characters():
+    client = TestClient(app)
+    response = client.post(
+        "/api/v1/tasks", json={"title": "A title", "description": 141 * "*"}
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_post_task_must_be_returned():
+    task_expected = {"title": "A title", "description": "A description"}
+
+    client = TestClient(app)
+    response = client.post("api/v1/tasks", json=task_expected)
+
+    task_received = response.json()
+
+    assert task_received["title"] == task_expected["title"]
+    assert task_received["description"] == task_expected["description"]
+
+    TASKS.clear()
+
+
+def test_post_task_must_be_a_unique_id():
+    task_1 = {"title": "First title", "description": "First description"}
+    task_2 = {"title": "Second title", "description": "Second description"}
+
+    client = TestClient(app)
+    response_1 = client.post("api/v1/tasks", json=task_1)
+    response_2 = client.post("api/v1/tasks", json=task_2)
+
+    assert response_1.json()["id"] != response_2.json()["id"]
+
+    TASKS.clear()
+
+
+def test_post_task_must_be_not_finalized_without_input():
+    task = {"title": "A title", "description": "A description"}
+
+    client = TestClient(app)
+    response = client.post("api/v1/tasks", json=task)
+
+    assert response.json()["status"] == "Not finalized"
+
+    TASKS.clear()
+
+
+def test_post_task_must_return_201_status_code():
+    task = {"title": "A title", "description": "A description"}
+
+    client = TestClient(app)
+    response = client.post("api/v1/tasks", json=task)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    TASKS.clear()
+
+
+def test_post_task_must_persists():
+    task = {"title": "A title", "description": "A description"}
+
+    client = TestClient(app)
+    client.post("api/v1/tasks", json=task)
+
+    assert len(TASKS) == 1
 
     TASKS.clear()
